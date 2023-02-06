79a9b91eec6a980726d745e1c21c49bb246a3250
Aiden Vigue <aiden@vigue.me>
2023-02-05 21:44:46 -0500

    search!!!

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -1,13 +1,41 @@
+"use client";
+
+import { useState } from "react";
 import { PostOrPage } from "@tryghost/content-api";
 
 import BlogPost from "@/components/BlogPost";
+import { FiSearch } from "react-icons/fi";
 
-export default function PostsList({ posts }: { posts: PostOrPage[] }) {
+export default function PostsList({ posts, search }: { posts: PostOrPage[], search: boolean }) {
+  const [searchValue, setSearchValue] = useState("");
+  const filteredPosts = posts
+    .sort((a, b) => {
+      return Date.parse(b.published_at) - Date.parse(a.published_at);
+    })
+    .filter((post) => {
+      return (
+        post.title.toLowerCase().includes(searchValue.toLowerCase()) || post.excerpt?.toLowerCase().includes(searchValue.toLowerCase())
+      );
+    });
+
   return (
     <>
-      {(!posts.length && <p className="my-8 mb-4 self-center text-gray-600 dark:text-gray-400">No posts found ;-;</p>) || (
+      {(search) && (
+        <div className="relative w-full">
+        <input
+          aria-label={`Search through ${filteredPosts.length} articles`}
+          type="text"
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder={`Search through ${filteredPosts.length} articles`}
+          className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-100"
+        />
+          <FiSearch className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300" />
+        </div>
+      )}
+
+      {(!filteredPosts.length && <p className="my-8 mb-4 self-center text-gray-600 dark:text-gray-400">No posts found ;-;</p>) || (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-          {posts.map((post) => (
+          {filteredPosts.map((post) => (
             <BlogPost key={post.slug} {...post} />
           ))}
         </div>
