330654c1755a647b907cf9d131ab45134011ef42
Aiden Vigue <aiden@vigue.me>
2023-02-12 17:42:46 -0500

    add video MIME

--- lib/render.tsx
+++ lib/render.tsx
@@ -5,6 +5,7 @@ export function GhostContent({ html }) {
   h = h.replaceAll(`<input type="range" class="kg-video-seek-slider" max="100" value="0">`,`<input type="range" class="kg-video-seek-slider" max="100" defaultValue="0">`);
   h = h.replaceAll(`<hr>`, `<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">`)
   h = h.replaceAll(`<img`, `<img class="rounded" loading="lazy"`)
+  h = h.replaceAll(`<video`, `<video type="video/mp4"`)
   return (
     <>
       <div className="gh-content" dangerouslySetInnerHTML={{__html: h}} />
