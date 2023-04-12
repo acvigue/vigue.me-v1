6b748286e47b5465c10c4b6976e5d7adc68b6a0d
Aiden Vigue <aiden@vigue.me>
2023-04-12 19:10:13 -0400

    add anna litics

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -7,6 +7,7 @@ export default function Analytics() {
   return (
     <>
       <VercelAnalytics />
+      <Script src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "18013156adf54ab583463d5a52fec7f3"}'></Script>
     </>
   );
 }
