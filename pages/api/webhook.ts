e7e3257f50635da2681e901981bc336495b0889b
Aiden Vigue <aiden@vigue.me>
2023-04-27 01:43:56 -0400

    try

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,4 +1,16 @@
+import { PostOrPage } from "@tryghost/content-api";
 import { NextApiRequest, NextApiResponse } from "next";
+
+interface WebhookBody {
+  post?: {
+    current?: PostOrPage;
+    previous?: PostOrPage;
+  };
+  page?: {
+    current?: PostOrPage;
+    previous?: PostOrPage;
+  };
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const secret = req.query.secret;
@@ -7,57 +19,80 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const body = req.body;
-  res.send(body);
-  console.log(body);
-  return;
+  const body: WebhookBody = req.body;
 
-  if (body.page) {
+  const paths = [];
+
+  if (body.page !== undefined) {
     const page = body.page;
     if (Object.keys(page.current).length === 0) {
       //page was deleted
-      await res.revalidate(`/${page.previous.slug}`);
-    } else {
-      await res.revalidate(`/${page.current.slug}`);
+      paths.push(`/${page.previous.slug}`);
 
-      if (page.previous.slug) {
-        await res.revalidate(`/${page.previous.slug}`);
+      if (page.previous.tags !== undefined) {
+        for (const tag of page.previous.tags) {
+          paths.push(`/tags/${tag.slug}`);
+        }
+      }
+    } else {
+      paths.push(`/${page.current.slug}`);
+
+      if (page.previous.slug != page.current.slug) {
+        paths.push(`/${page.previous.slug}`);
+      }
+
+      let tag_slugs: [String?] = [];
+      for (const tag of [...page.previous.tags, ...page.current.tags]) {
+        tag_slugs.push(tag.slug);
+      }
+
+      const unique_tag_slugs = [...new Set(tag_slugs)];
+
+      for (const tag of unique_tag_slugs) {
+        paths.push(`/tags/${tag}`);
       }
     }
-  } else if (body.post) {
+  } else if (body.post !== undefined) {
     const post = body.post;
     if (Object.keys(post.current).length === 0) {
       //post was deleted
-      if (post.previous.tags) {
-        post.previous.tags.forEach(async (tag) => {
-          await res.revalidate(`/tags/${tag.slug}`);
-        });
+      if (post.previous.tags !== undefined) {
+        for (const tag of post.previous.tags) {
+          paths.push(`/tags/${tag.slug}`);
+        }
       }
-      await res.revalidate(`/posts`);
+      paths.push(`/posts`);
 
-      await res.revalidate(`/posts/${post.previous.slug}`);
+      paths.push(`/posts/${post.previous.slug}`);
     } else {
-      await res.revalidate(`/posts/${post.current.slug}`);
-      await res.revalidate(`/posts`);
+      paths.push(`/posts/${post.current.slug}`);
 
-      if (post.current.tags) {
-        post.current.tags.forEach(async (tag) => {
-          await res.revalidate(`/tags/${tag.slug}`);
-        });
+      let tag_slugs: [String?] = [];
+      for (const tag of [...post.previous.tags, ...post.current.tags]) {
+        tag_slugs.push(tag.slug);
       }
 
-      if (post.previous.tags) {
-        post.previous.tags.forEach(async (tag) => {
-          await res.revalidate(`/tags/${tag.slug}`);
-        });
+      const unique_tag_slugs = [...new Set(tag_slugs)];
+
+      for (const tag of unique_tag_slugs) {
+        paths.push(`/tags/${tag}`);
       }
 
-      if (post.previous.slug) {
-        await res.revalidate(`/posts/${post.previous.slug}`);
+      if (post.previous.slug !== post.current.slug) {
+        paths.push(`/posts/${post.previous.slug}`);
       }
+
+      paths.push(`/posts`);
     }
   } else {
-    res.status(400).send({ error: "no_focus" });
+    res.status(400).send({ error: "invalid_body" });
+    return;
+  }
+
+  if(paths.length > 0) {
+    console.log(paths);
+    res.status(200).send({ revalidated: paths });
+    //await Promise.all(paths.map(path => res.revalidate(path)));
   }
 }
 
