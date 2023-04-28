62c2d7bed95e33d006652ee7187503df50c9bfd5
Aiden Vigue <aiden@vigue.me>
2023-04-28 11:21:53 -0400

    ghost lib return mobiledoc from admin API

--- lib/ghost.ts
+++ lib/ghost.ts
@@ -1,20 +1,32 @@
 import { cache } from "react";
 
 import GhostContentAPI, { Params, PostOrPage, PostsOrPages, Tag, Tags } from "@tryghost/content-api";
+import GhostAdminAPI from "@tryghost/admin-api";
 
 // Create API instance with site credentials
-const api = new GhostContentAPI({
+const contentAPI = new GhostContentAPI({
   url: process.env.GHOST_URL,
   key: process.env.GHOST_CONTENT_KEY,
   ghostPath: process.env.GHOST_PATH,
   version: "v5.0"
 });
 
+const adminAPI = new GhostAdminAPI({
+  url: process.env.GHOST_URL,
+  key: process.env.GHOST_ADMIN_KEY,
+  ghostPath: process.env.GHOST_PATH,
+  version: "v5.0"
+});
+
+interface PostOrPageWithMobiledoc extends PostOrPage {
+  mobiledoc: Object
+}
+
 export const getPages = cache(_getPages);
 async function _getPages(filters: Params) {
   let x: Params = {...filters, include: ["tags", "authors"]};
   try {
-    return (await api.pages.browse(x)) as PostsOrPages;
+    return (await contentAPI.pages.browse(x)) as PostsOrPages;
   } catch(e) {
     console.error(`[ghost] getPages error: ${e}`);
     return [] as PostsOrPages;
@@ -25,7 +37,7 @@ export const getPosts = cache(_getPosts);
 async function _getPosts(filters: Params) {
   let x: Params = {...filters, include: ["tags", "authors"]};
   try {
-    return (await api.posts.browse(x)) as PostsOrPages;
+    return (await contentAPI.posts.browse(x)) as PostsOrPages;
   } catch(e) {
     console.error(`[ghost] getPosts error: ${e}`);
     return [] as PostsOrPages;
@@ -36,7 +48,7 @@ export const getTags = cache(_getTags);
 async function _getTags(filters: Params) {
   let x: Params = {...filters};
   try {
-    return (await api.tags.browse(x)) as Tags;
+    return (await contentAPI.tags.browse(x)) as Tags;
   } catch(e) {
     console.error(`[ghost] getTags error: ${e}`);
     return [] as Tags;
@@ -46,7 +58,7 @@ async function _getTags(filters: Params) {
 export const getTag = cache(_getTag);
 async function _getTag(tag_id?: string, tag_slug?: string) {
   try {
-    return (await api.tags.read({id: tag_id, slug: tag_slug})) as Tag;
+    return (await contentAPI.tags.read({id: tag_id, slug: tag_slug})) as Tag;
   } catch(e) {
     console.error(`[ghost] getTag error: ${e}`);
     return [] as Tags;
@@ -56,19 +68,25 @@ async function _getTag(tag_id?: string, tag_slug?: string) {
 export const getPage = cache(_getPage);
 async function _getPage(page_id?: string, page_slug?: string) {
   try {
-    return (await api.pages.read({id: page_id, slug: page_slug})) as PostOrPage;
+    let main = await contentAPI.pages.read({id: page_id, slug: page_slug}) as PostOrPageWithMobiledoc
+    const adminPage = await adminAPI.pages.read({id: page_id, slug: page_slug});
+    main.mobiledoc = adminPage.mobiledoc;
+    return main;
   } catch(e) {
     console.error(`[ghost] getPage error: ${e}`);
-    return {} as PostOrPage;
+    return {} as PostOrPageWithMobiledoc;
   }
 }
 
 export const getPost = cache(_getPost);
 async function _getPost(post_id?: string, post_slug?: string) {
   try {
-    return (await api.posts.read({id: post_id, slug: post_slug}, { include: ['authors', 'tags'] })) as PostOrPage;
+    let main = await contentAPI.posts.read({id: post_id, slug: post_slug}, { include: ['authors', 'tags'] }) as PostOrPageWithMobiledoc
+    const adminPost = await adminAPI.posts.read({id: post_id, slug: post_slug});
+    main.mobiledoc = adminPost.mobiledoc;
+    return main;
   } catch(e) {
     console.error(`[ghost] getPost error: ${e}`);
-    return {} as PostOrPage;
+    return {} as PostOrPageWithMobiledoc;
   }
 }
