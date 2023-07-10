28e2beed419f5b7c8c8da871c5b70a59cdf96789
Aiden Vigue <aiden@vigue.me>
2023-07-10 16:57:02 -0400

    fix mobiledoc rendering of markups

--- components/GhostRenderer.tsx
+++ components/GhostRenderer.tsx
@@ -1,4 +1,4 @@
-import Renderer from 'react-mobiledoc-renderer';
+import Renderer, { MobiledocInput } from 'react-mobiledoc-renderer';
 import "styles/mobiledoc.scss";
 
 import cards from './mobiledoc/cards';
@@ -20,12 +20,31 @@ const renderer = new Renderer(options);
 
 export default async function GhostRenderer(props: GhostRendererProps) {
 
-    let mobiledoc = props.mobiledoc;
+    let mobiledoc = props.mobiledoc as MobiledocInput;
+
+    //Ghost has issues.
+    for (const [si, section] of mobiledoc.sections.entries()) {
+        try {
+            if (section.length > 2) {
+                const markups = section[2];
+                if ((section[2] as any[]).length === 0) {
+                    continue;
+                }
+                if (markups[0].length === 1) {
+                    for (const [mi, markup] of (markups as any[]).entries()) {
+                        mobiledoc.sections[si][2][mi] = markup[0];
+                    }
+                }
+            }
+        } catch (e) {
+            console.log(section);
+        }
+    }
 
     try {
         const { result } = await renderer.render(mobiledoc);
         return result
-    } catch(e) {
+    } catch (e) {
         console.log('Mobiledoc render error: ', e, mobiledoc);
         return (<p>Rendering error</p>)
     }
