45d34e163d2be12f706928cab27f81005da60594
Aiden Vigue <aiden@vigue.me>
2023-07-11 14:49:18 -0400

    work on images, fix lists, fix gigantic bookmarks

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function Page() {
         <div className="text-lg mt-2 dark:text-gray-400 text-gray-700 md:text-xl font-medium leading-tight antialiased" dangerouslySetInnerHTML={{__html: data.page.html}}/>
         <div className="flex flex-start flex-shrink">
           <div className="text-white drop-shadow-2xl relative transform-gpu duration-300 group">
-            <div className="absolute w-full h-full transform-gpu dark:mix-blend-overlay bg-gray-600 opacity-20 dark:opacity-25 -rotate-6 rounded-lg -z-10 duration-300 group-hover:rotate-0 group-hover:scale-90"></div>
+            <div className="absolute w-full h-full transform-gpu dark:mix-blend-overlay bg-gray-400 opacity-20 dark:opacity-25 -rotate-6 rounded-lg -z-10 duration-300 group-hover:rotate-0 group-hover:scale-90"></div>
             <Link href={`mailto:${config.email}`} className="text-sm font-bold uppercase flex flex-row items-center p-3 bg-pink-600 rounded-lg transform-gpu duration-300 group-hover:scale-90">
               Get in touch <FiMail className="ml-2"></FiMail>
             </Link>
