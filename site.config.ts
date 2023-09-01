0efb12247ea0ed8d1ef70319ab1a18c01ae48de0
Aiden Vigue <aiden@vigue.me>
2023-09-01 16:09:34 -0400

    add linkedin

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ export const config = {
   shortName: "Aiden",
   roles: ["I'm a Software Engineer.", "I create websites.", "I design user interfaces.", "I'm an eager learner.", "I make things."],
   headshot: "https://images.weserv.nl/?url=https://github.com/acvigue.png&h=300&w=300&fit=cover&mask=circle&l=9&output=webp&q=63",
-  description: "I'm a high school senior from Virginia who's interested in computer science with a focus on embedded engineering.",
+  description: "I'm a freshman at Purdue studying Computer Science with a focus on embedded engineering.",
   email: "aiden@vigue.me",
   username: "acvigue",
   baseUrl: "https://vigue.me",
@@ -15,7 +15,7 @@ export const config = {
   nav: [
     { name: "About", href: "/about" },
     { name: "Posts", href: "/posts" },
-    { name: "Resume", href: "https://dnzye6trx9wog.cloudfront.net/Resume.pdf" },
+    { name: "Resume", href: "/resume" },
   ],
   footer: {
     links: [],
@@ -29,6 +29,11 @@ export const config = {
         name: "RSS Feed",
         icon: "FiRss",
         href: "https://vigue.me/feed.xml",
+      },
+      {
+        name: "LinkedIn",
+        icon: "FiLinkedIn",
+        href: "https://linkedin.com/in/aidenvigue",
       },
     ],
   },
